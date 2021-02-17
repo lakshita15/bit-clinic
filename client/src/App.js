733f968c5183487ae3f84163654911fd14bb06c3
lakshita15 <lakshitamahajan2001@gmail.com>
2021-02-17 13:05:27 +0530

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import 
+  Login
+ from "./Components/Login/Login.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Login />
+      
+      <div className="safe-space" />
+      <Router>
+        <Switch>
+         
+          <Route path="/Login" exact component={Login}></Route>
+          
+         
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ client/src/Components/Register/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Register.scss"
+class Register extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div></div>
+         );
+    }
+}
+ 
+export default Register;
